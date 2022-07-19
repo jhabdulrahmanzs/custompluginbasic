8672fc61e4e001e7c0dfe8bff9d77fad5d6ee947
jhabdulrahmanzs <abdulrahman.jawad@zucisystems.com>
2022-07-19 13:18:55 +0530

    added style nd script in wp format

--- assets/public.php
+++ assets/public.php
@@ -1,10 +1,33 @@
 <?php
-function dev_add_scripts(){
-    // Add main CSS
-    wp_enqueue_style( 'dev-main-style', plugins_url(). '/myplugin/assets/css/style.css');
-    // Add main JS
-    wp_enqueue_script( 'dev-main-scripts', plugins_url(). '/myplugin/assets/js/main.js');
 
+function ui_add_scripts(){
+    // Add main CSS
+    wp_enqueue_style( 'main-style', trailingslashit( MY_PLUGIN_URL ). 'assets/css/style.css');
+    // bootstrap css
+    wp_enqueue_style( 'minCSSbOOTSTRAP', trailingslashit( MY_PLUGIN_URL ). 'assets/vendor/bootstrap/css/bootstrap.min.css');
+    // font awesome css
+    wp_enqueue_style( 'minCSSfontawesome', trailingslashit( MY_PLUGIN_URL ). 'assets/vendor/fontawesome/css/font-awesome.min.css');
+    // Add main JS
+    wp_enqueue_script( 'main-script', trailingslashit( MY_PLUGIN_URL ). 'assets/js/main.js');
+    // bootstrap js
+    wp_enqueue_script( 'bubbleJSbOOTSTRAP', trailingslashit( MY_PLUGIN_URL ). 'assets/vendor/bootstrap/js/bootstrap.bundle.js');
 }
 
-add_action('wp_enqueue_scripts','dev_add_scripts');
+add_action('wp_enqueue_scripts','ui_add_scripts'); 
+
+
+function admin_add_scripts(){
+    // Add main CSS
+    wp_enqueue_style( 'main-style', trailingslashit( MY_PLUGIN_URL ). 'assets/css/admin/style.css');
+    // bootstrap css
+    wp_enqueue_style( 'minCSSbOOTSTRAP', trailingslashit( MY_PLUGIN_URL ). 'assets/vendor/bootstrap/css/bootstrap.min.css');
+    // font awesome css
+    wp_enqueue_style( 'minCSSfontawesome', trailingslashit( MY_PLUGIN_URL ). 'assets/vendor/fontawesome/css/font-awesome.min.css');
+    // Add main JS
+    wp_enqueue_script( 'main-script', trailingslashit( MY_PLUGIN_URL ). 'assets/js/admin/main.js');
+    // bootstrap js
+    wp_enqueue_script( 'bubbleJSbOOTSTRAP', trailingslashit( MY_PLUGIN_URL ). 'assets/vendor/bootstrap/js/bootstrap.bundle.js');
+}
+
+add_action('admin_enqueue_scripts', 'admin_add_scripts');
+// echo "ffffffffffffffffff";
