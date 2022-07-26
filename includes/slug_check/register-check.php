ade237296049d1a24eacb29d343f34707e3f642e
Pravin Murugandi <pravin.murugandi@zucisystems.com>
2022-07-26 19:14:11 +0530

    class convertion

--- includes/slug_check/register-check.php
+++ includes/slug_check/register-check.php
@@ -1,60 +1,64 @@
 <?php
-    $gethomeurl =get_home_url();
+if (!class_exists('register_check')) {
+    class register_check
+    {
+        function __construct()
+        {
+            $this->slug_register();
+        }
+        public function slug_register()
+        {
 
-    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    $getslugname = str_replace($gethomeurl,'',$actual_link);
-    $rmslash = preg_replace('|/|', '',$getslugname);
-    
-    if($rmslash == 'newregistration') {
-       
-        add_filter('the_content', 'register_create_page');
-    }      
-        
-      
-    function register_create_page($content)
-    {      
-        include_once(ROOTDIR . '/templates/registeration.php');
-        return $content;
+            $gethomeurl = get_home_url();
+
+            $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+            $getslugname = str_replace($gethomeurl, '', $actual_link);
+            $rmslash = preg_replace('|/|', '', $getslugname);
+
+            if ($rmslash == 'newregistration') {
+
+                add_filter('the_content', [$this, 'register_create_page']);
+            }
+        }
+        function register_create_page($content)
+        {
+            include_once(ROOTDIR . '/templates/registeration.php');
+            return $content;
+        }
     }
-
-
-
-function ajax_registerform(){
-    $firstname = $_POST['firstname'];    
+}
+function ajax_registerform()
+{
+    $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
     $age = $_POST['age'];
     $contact = $_POST['contact'];
     $address = $_POST['address'];
     $password = $_POST['password'];
-       
-require_once(ROOTDIR. '/models/registerdata_model.php');
-$register=new registeration();
-$register->create_user($firstname,$password,$email);
-$registerform=array(
-    'firstname' => $firstname,
-    'lastname' => $lastname,
-    'email' => $email,
-    'age' => $age,
-    'contact' => $contact,
-    'address' => $address,
-    'password' => $password
-);
-$registerform_datatype=array(
-    '%s',
-    '%s',
-    '%s',
-    '%d',
-    '%d',
-    '%s',
-    '%s'
-);
-$register->insert_data($registerform,$registerform_datatype);
 
-   
-} 
-add_action( 'wp_ajax_ajax_registerform', 'ajax_registerform' );
+    require_once(ROOTDIR . '/models/registerdata_model.php');
+    $register = new registeration();
+    $register->create_user($firstname, $password, $email);
+    $registerform = array(
+        'firstname' => $firstname,
+        'lastname' => $lastname,
+        'email' => $email,
+        'age' => $age,
+        'contact' => $contact,
+        'address' => $address,
+        'password' => $password
+    );
+    $registerform_datatype = array(
+        '%s',
+        '%s',
+        '%s',
+        '%d',
+        '%d',
+        '%s',
+        '%s'
+    );
+    $register->insert_data($registerform, $registerform_datatype);
+}
+add_action('wp_ajax_ajax_registerform', 'ajax_registerform');
 // add_action('wp_ajax_nopriv_registerform', 'registerform');
-
-?>
-
--- templates/page_creation/register-page-create.php
+++ templates/page_creation/register-page-create.php
@@ -1,31 +1,40 @@
 <?php
 
 
-define( 'PLUGIN_FILE_PATH', __FILE__ );
+define('PLUGIN_FILE_PATH', __FILE__);
+if (!class_exists('registerpage')) {
+    class registerpage
+    {
 
-register_activation_hook( 'PLUGIN_FILE_PATH', 'register_page_on_activation' );
+        function __construct()
+        {
+            register_activation_hook(__FILE__,  [$this, 'register_page_on_activation']);
+            // add_action('wp_head', [$this, 'create_plugin_database_table']);
+            add_filter('the_content', [$this, 'pagecreate_register']);
+        }
 
-function register_page_on_activation() {
-     if ( ! current_user_can( 'activate_plugins' ) ) return;
+        function register_page_on_activation()
+        {
+            if (!current_user_can('activate_plugins')) return;
+        }
+        public function pagecreate_register()
+        {
+            $post_values = '';
+            // $PageGuid = site_url() . "/regsiterdata";
+            $page_slug = 'newregistration'; // Slug of the Post
+            $new_page = array(
+                'post_type'     => 'page',                 // Post Type Slug eg: 'page', 'post'
+                'post_title'    => 'Registration',    // Title of the CoFntent
+                'post_content'  => $post_values,    // Content
+                'post_status'   => 'publish',            // Post Status
+                'post_author'   => 1,                    // Post Author ID
+                'post_name'     => $page_slug,        // Slug of the Post
+                'menu_order'     => 0,
+            );
+
+            if (!get_page_by_path($page_slug, OBJECT, 'page')) { // Check If Page Not Exits
+                $new_page_id = wp_insert_post($new_page);
+            }
+        }
+    }
 }
-
-$post_values= '';
-// $PageGuid = site_url() . "/regsiterdata";
-$page_slug = 'newregistration'; // Slug of the Post
-$new_page = array(
-    'post_type'     => 'page', 				// Post Type Slug eg: 'page', 'post'
-    'post_title'    => 'Registration',	// Title of the CoFntent
-    'post_content'  => $post_values,	// Content
-    'post_status'   => 'publish',			// Post Status
-    'post_author'   => 1,					// Post Author ID
-    'post_name'     => $page_slug,		// Slug of the Post
-    'menu_order'     => 0,
-);
-
-if (!get_page_by_path( $page_slug, OBJECT, 'page')) { // Check If Page Not Exits
-    $new_page_id = wp_insert_post($new_page);
-}
-
-
-
-?>
